9565756a9809234f9b29e7b33abe3b18341153ab
Irakliy <irakliy81@yahoo.com>
2017-08-15 20:59:54 -0700

    updated dependencies

--- bin/index.js
+++ bin/index.js
@@ -31,13 +31,7 @@ class IdGenerator extends events.EventEmitter {
         this.cacheWindow = options.window || DEFAULT_CACHE_WINDOW;
         // error in redis connection should not bring down the service
         this.client.on('error', (error) => {
-            if (error.command === 'AUTH' && error.code === 'UNCERTAIN_STATE') {
-                // this will be triggered on recconect attempts - do nothing
-                this.logger && logger.warn('Suppressing AUTH command error on reconnect', this.name);
-            }
-            else {
-                this.emit(ERROR_EVENT, new IdGeneratorError(error, 'ID Generator error'));
-            }
+            this.emit(ERROR_EVENT, new IdGeneratorError(error, 'ID Generator error'));
         });
     }
     getNextId() {
--- index.ts
+++ index.ts
@@ -76,13 +76,7 @@ export class IdGenerator extends events.EventEmitter {
 
         // error in redis connection should not bring down the service
         this.client.on('error', (error) => {
-            if (error.command === 'AUTH' && error.code === 'UNCERTAIN_STATE') {                
-                // this will be triggered on recconect attempts - do nothing
-                this.logger && logger.warn('Suppressing AUTH command error on reconnect', this.name);
-            }
-            else {
-                this.emit(ERROR_EVENT, new IdGeneratorError(error, 'ID Generator error'));
-            }
+            this.emit(ERROR_EVENT, new IdGeneratorError(error, 'ID Generator error'));
         });
 	}
     
